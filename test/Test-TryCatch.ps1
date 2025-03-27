418a2f957e851addc4c5bb3291f23b1a5e29c89a
chiku-wa <chikuwa.wachi@gmail.com>
2025-03-28 08:27:45 +0900

    README.mdに命名規則などを追記。

---
+++ test/Test-TryCatch.ps1
@@ -0,0 +1,14 @@
+<#
+.SYNOPSIS
+    指定した名前を表示する関数
+
+.DESCRIPTION
+    Say-Hello関数は、ユーザーが指定した名前をコンソールに表示する
+
+.PARAMETER Name
+    挨拶する対象の名前
+
+.EXAMPLE
+    Say-Hello -Name "Taro"
+    Hello, Taro!
+#>
